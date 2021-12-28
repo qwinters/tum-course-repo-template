8629f0146cc8b5903a9d1ead26902bbb65275a7c
qwinters <whisks.wafers-0l@icloud.com>
2021-12-28 15:32:33 +0100

    feat(gh-pages): Moving gh-pages asset location

--- .github/scripts/encrypt.sh
+++ .github/scripts/encrypt.sh
@@ -4,16 +4,18 @@ CURR_DIR="$(PWD)"
 REPO_DIR="$(git rev-parse --show-toplevel)"
 
 cd $REPO_DIR
-if [[ ! -d encrypted ]] ; then 
-    mkdir -p encrypted
-fi 
 
-cp -r homework*/*.pdf encrypted
-
-cd encrypted
-for pdffile in *.pdf ; do 
-    encfile="$(basename $pdffile .pdf)-enc.pdf"
-    qpdf --encrypt $HOMEWORK_PASSWORD $HOMEWORK_PASSWORD 256 -- $pdffile $encfile
-    rm -rf $pdffile
+for assignment in homework* ; do 
+    cd $assignment
+    for pdffile in *.pdf ; do 
+        encfile="$(basename $pdffile .pdf)-enc.pdf"
+        qpdf --encrypt $HOMEWORK_PASSWORD $HOMEWORK_PASSWORD 256 -- $pdffile $encfile
+    done 
+    cd .. 
+    if [[ ! -d gh-pages/"$assigment" ]] ; then 
+        mkdir -p gh-pages/"$assignment"
+    fi
+    mv "$assignment"/*-enc.pdf gh-pages/"$assignment"
 done 
+
 cd $CURR_DIR
