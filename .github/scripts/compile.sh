5b11be73deef1faa7438503a7bec7539e0cd1ae4
qwinters <whisks.wafers-0l@icloud.com>
2022-04-23 16:24:13 +0200

    feat(ci): Add build directory + refactor compile script

--- .github/scripts/compile.sh
+++ .github/scripts/compile.sh
@@ -1,17 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env sh 
 
-# Compile homework assignments
-for assignment in homework* ; do 
-    cd $assignment
-    for file in *.tex ; do 
-        latexmk -f -pdf -interaction=nonstopmode $file
-    done 
-    cd .. 
+REPO_DIR="$(git rev-parse --show-toplevel)"
+
+if [[ -z "$FILE_PREFIX" ]] ; then 
+  files_to_move=(notes.tex ex-sheets.tex ex-solns.tex outline.tex)
+  for file in $files_to_move ; do 
+    if [[ -f "$REPO_DIR/$file" ]] ; then 
+      mv "$REPO_DIR/$file" "$REPO_DIR/$FILE_PREFIX-$file"
+    fi
+  done
+fi 
+
+for file in $(ls $REPO_DIR/*.tex) ; do 
+  latexmk -f -pdf -interaction=nonstopmode -outdir=.build $file
 done
 
-# Compile notes
-cd notes
-for file in *.tex ; do 
-    latexmk -f -pdf -interaction=nonstopmode $file
-done
-cd ..
+git add -f "$REPO_DIR"/.build/*.pdf
+git add -f "$REPO_DIR"/*.tex 
+
+if [[ ! -z $(git status --porcelain) ]] ; then 
+  git config --global user.name "GitHub Action"
+  git commit -m "chore(LaTeX): Compile and update PDFs"
+  git push
+fi 
